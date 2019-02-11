52ce04804aadcfbe9f10c5da4794720e1ba006e5
Christopher Wenceslaus <topher.wenceslaus@gmail.com>
2019-02-11 21:54:29 +0530

    Create Country.js

---
+++ src/Country.js
@@ -0,0 +1,42 @@
+let countries= [{
+  id: 0,
+  title: 'US',
+  code: '1',
+  selected: false,
+  key: 'country',
+  flag:'https://einfon.com/wp-content/uploads/2017/05/Flag-Of-USA.jpg'
+},
+{
+  id: 1,
+  title: 'UK',
+  code: '44',
+  selected: false,
+  key: 'country',
+  flag:'https://einfon.com/wp-content/uploads/2017/05/Flag-Of-USA.jpg'
+},
+{
+  id: 2,
+  title: 'IND',
+  code: '91',
+  selected: false,
+  key: 'country',
+  flag:'https://einfon.com/wp-content/uploads/2017/05/Flag-Of-USA.jpg'
+},
+{
+  id: 3,
+  title: 'NZ',
+  selected: false,
+  code: '33',
+  key: 'country',
+  flag:'https://einfon.com/wp-content/uploads/2017/05/Flag-Of-USA.jpg'
+},
+{
+  id: 4,
+  title: 'AUS',
+  selected: false,
+  code: '88',
+  key: 'country',
+  flag:'https://einfon.com/wp-content/uploads/2017/05/Flag-Of-USA.jpg'
+}]
+
+export default countries
